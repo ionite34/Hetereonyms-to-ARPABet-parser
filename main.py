a5af056507e10db8d8a013924ef2289253e483bd
ionite <ionite@pm.me>
2022-03-28 22:15:14 -0400

    Converted to support new g2p_h replace method

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if not isDev:
 # imports
 from plugins.hetereonyms_to_arpabet.g2p_h import G2p
 import datetime
+import re
 
 logger = setupData["logger"]
 
@@ -15,6 +16,7 @@ def het_to_arpabet(data=None):
     global logger
     global G2p
     global datetime
+    global re
     
     # Grab original text line
     text_line = data["sentence"]
@@ -27,16 +29,33 @@ def het_to_arpabet(data=None):
     # Start timer
     start_time = datetime.datetime.now()
 
-    # Convert to ARPABet
+    # Starts G2p
     g2p = G2p()
-    # Get converted string
-    converted_line = g2p(text_line)
-    logger.log(f'converted_line: {converted_line}')
-    # convert the converted_string list to a string and store
-    converted_string_joined = ''.join(converted_line)
-    logger.log(f'converted_string_joined: {converted_string_joined}')
-    # modify the sentence
-    data["sentence"] = converted_string_joined
+
+    # Check if there is a heteronym in the text using the heteronyms.en dictionary in the g2p_h subfolder
+    if g2p.contains_het(text_line):
+        # If there is, get the replacement list of homographs and their phonemes
+        source_list = g2p.het_replace(text_line)
+        originals, replacements, typeWord = source_list
+        logger.log(f'originals: {originals}')
+        logger.log(f'replacements: {replacements}')
+        logger.log(f'Heteronym types: {typeWord}')
+        # Replace the original words in the text with the homograph phonemes
+        for index, original_word in enumerate(originals):
+            logger.log(f'index: [{index}]')
+            logger.log(f'original word: <{original_word}>')
+            # build the replacement string
+            # needs to be enclosed by curly brackets, and each phoneme is space separated
+            replacement_string = '{' + ' '.join(replacements[index]) + '}'
+            logger.log(f'replacement string: <{replacement_string}>')
+            # match the original word as a whole word only using regex (\b) and replace
+            text_line = re.sub(r'\b' + original_word + r'\b', replacement_string, text_line, 1)
+        # Add a space to the beginning and end of the text line
+        text_line = '{ } ' + text_line + ' { }'
+        # Set the data to the new text line
+        data["sentence"] = text_line
+        # Log the new text line
+        logger.log(f'Modified line: {text_line}')
 
     # End timer
     end_time = datetime.datetime.now()
